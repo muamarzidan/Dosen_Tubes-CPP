a0dda569321a03d3fb1d8642df4f933610089bfd
Faiz Elfahad Kurniawan <74404911+siunlucky@users.noreply.github.com>
2024-12-02 05:59:10 +0700

    upd: adt

--- mll.h
+++ mll.h
@@ -1,6 +1,50 @@
 #ifndef MLL_H
 #define MLL_H
-#include <iostream>
+
+#define first(L) L.first
+#define last(L) L.last
+#define prev(P) P->prev
+#define next(P) P->next
+#define info(P) P->info
+#define mataKuliah(P) P->mataKuliah
+
+using namespace std;
+
+struct dosen {
+    string nama;
+    string kode;
+    string gender;
+};
+
+typedef dosen infotypeDosen;
+typedef struct elemenDosen *addressDosen;
+
+struct mataKuliah {
+    string nama;
+    string kode;
+    int sks;
+};
+
+typedef mataKuliah infotypeMataKuliah;
+typedef struct elemenMataKuliah *addressMataKuliah;
+
+struct elemenMataKuliah {
+    addressMataKuliah prev;
+    infotypeMataKuliah info;
+    addressMataKuliah next;
+};
+
+struct elemenDosen {
+    addressDosen prev;
+    infotypeDosen info;
+    addressMataKuliah mataKuliah;
+    addressDosen next;
+};
+
+struct ListDosen {
+    addressDosen first;
+    addressDosen last;
+};
 
 int tambah(int a, int b);
 void menu();
